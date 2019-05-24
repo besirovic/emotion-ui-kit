66c49f7aab3a17d363dbb272b5d73349ff8c474c
Ademir Besirovic <ademir.besirovic1996@gmail.com>
2019-05-24 15:10:20 +0200

    create base theme

---
+++ src/themes/base/index.js
@@ -0,0 +1,23 @@
+export default {
+  colors: {
+    primary: '#7D4CDB',
+    secondary: '#FD6FFF',
+    light: '#EDEDED',
+    dark: '#333'
+  },
+  spacing: {
+    small: 4,
+    medium: 8,
+    large: 12
+  },
+  borderSize: {
+    small: 1,
+    medium: 2,
+    large: 4
+  },
+  edgeSize: {
+    small: 2,
+    medium: 4,
+    large: 6
+  }
+};
