ce97c0e9ae3e370c7f2d6e282f302c695a98d180
Ademir Besirovic <ademir.besirovic1996@gmail.com>
2019-05-24 15:10:09 +0200

    setup storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,10 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+import { addReadme } from 'storybook-readme';
+
+import globalStyleDecorator from './globalStyleDecorator';
+
+addDecorator(addReadme());
+addDecorator(globalStyleDecorator);
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../src', true, /\.stories\.js$/);
